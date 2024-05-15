b51f033d8bd6aa88ab7f045e3034718853a71f06
jbower34 <jbower34@ford.com>
2024-05-15 13:34:43 +0100

    disable WebpUnsupported

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    lint {
+        disable += "WebpUnsupported"
+    }
 }
-
 dependencies {
     implementation(project(":features:feature-base"))
     val navVersion = "2.7.7"
